1e28bde3e1863268790aacd11accd2fc243af98d
xdk78 <xdk78@interia.pl>
2018-05-09 22:14:53 +0200

    Parse date

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,23 +1,30 @@
 <template>
   <v-card class="my-1">
     <v-card-text>
-        <div class="grey--text" >
-          Anonymous <span class="black--text" ><b>{{post.title.toUpperCase()}}</b></span>
+        <div>
+          <div class="grey--text" >Anonymous</div> <span>{{post.title.toUpperCase()}}</span>
         </div>
         <v-spacer />
         <p class="mt-3">
-        {{post.content}}
-        </p>
+            {{post.content}}
+          </p>
+        <div v-text="parseDate(post.timestamp.seconds)"></div>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import {format} from 'date-fns'
 export default {
   name: 'Post',
   props: ['post'],
   data () {
     return {
+    }
+  },
+  methods: {
+    parseDate (time) {
+      return format(time)
     }
   }
 }
